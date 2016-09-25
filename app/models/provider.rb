132fe4551649ba13e36bc8fe30dc752c053abff0
Kaoru Kohashigawa <dev@kaoruk.com>
2016-09-25 10:53:46 -0700

    add scope to order provider by nearest

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,2 +1,16 @@
 class Provider < ActiveRecord::Base
+  scope :with_provider_type, -> (type) {
+    return self if type.nil?
+
+    where(provider_type: type)
+  }
+
+  scope :order_by_closest_to, -> (zipcode) {
+    lat, lon = MS_ZIPCODES[zipcode.to_s].slice("lat", "long").values
+    order(
+      "providers.coordinates::geometry <-> "\
+      "ST_SetSRID(ST_MakePoint(#{lon}, #{lat}), 4326)::geometry"
+    )
+  }
+
 end
